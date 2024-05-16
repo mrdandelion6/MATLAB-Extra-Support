87646b96db972596a9d33c76e8acd4bb4bbabb2d
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-05-15 20:19:31 -0400

    created an implemented the run section command for the extension

--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,95 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "matlab-extra-support" is now active!');
+	const outputChannel = vscode.window.createOutputChannel('MATLAB Extra Support');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('matlab-extra-support.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MATLAB Extra Support!');
+	let disposable1 = vscode.commands.registerCommand('matlab-extra-support.runSection', async () => {
+		
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document; // retrieve the document the editor is on
+			if (document.languageId === 'matlab') {
+
+				const cursorPosition = editor.selection.active;
+				let startSectionLine = cursorPosition.line;
+				let endSectionLine = cursorPosition.line;
+
+				// first we will find the start of the section
+				while (startSectionLine > 0 && !document.lineAt(startSectionLine).text.startsWith('%%')) {
+					startSectionLine--;
+				}
+
+				// now find the end
+				while(endSectionLine < document.lineCount - 1 && !document.lineAt(endSectionLine).text.startsWith('%%')) {
+					endSectionLine++;
+				}
+
+				const sectionRange = new vscode.Range(startSectionLine, 0, endSectionLine, document.lineAt(endSectionLine).text.length);
+        		let sectionText = document.getText(sectionRange);
+				
+				// trim the section text and remove the '%%' from the start and end
+				sectionText = sectionText.replace(/^%%|%%$/g, '').trim();
+
+				// check if the MATLAB terminal is running. if not we start one and execute our code
+				let matlabTerminal = null;
+
+				for (let terminal of vscode.window.terminals) {
+					if (terminal.name === "MATLAB") {
+						matlabTerminal = terminal;
+						break;
+					}
+				}
+
+				if (matlabTerminal) {
+					outputChannel.appendLine("MATLAB terminal is running");
+					// send the code to the terminal
+					try {
+						matlabTerminal.sendText(sectionText);
+					} catch (error) {
+						vscode.window.showErrorMessage("Error sending code to MATLAB terminal");
+						return;
+					}
+
+				} else {
+					outputChannel.appendLine("MATLAB terminal is not running. Started one");
+
+					// start the MATLAB terminal
+					try { 
+						await vscode.commands.executeCommand('matlab.openCommandWindow');
+					}
+					catch (error) {
+						vscode.window.showErrorMessage("Error starting MATLAB terminal. Ensure MATLAB Extension by MathWorks is installed.");
+						return;
+					}
+
+					// retrieve the terminal we just created
+					for (let terminal of vscode.window.terminals) {
+						if (terminal.name === "MATLAB") {
+							matlabTerminal = terminal;
+						}
+					}
+
+					// error checking
+					if (matlabTerminal === null) {
+						vscode.window.showErrorMessage("Cannot retrieve created MATLAB terminal");
+						return;
+					}
+
+					// send the code to the terminal
+					try {
+						matlabTerminal.sendText(sectionText);
+					} catch (error) {
+						vscode.window.showErrorMessage("Error sending code to MATLAB terminal");
+						return;
+					}
+
+				}
+			}
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable1);
 }
 
 // This method is called when your extension is deactivated
---
+++ test.m
@@ -0,0 +1,8 @@
+
+disp("yoskies1")
+%%
+disp("yoskies2")
+
+% help
+disp("1234")
+% ok
